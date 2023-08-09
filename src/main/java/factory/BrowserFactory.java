6253379f7b3ac5f1742e4707625109ed9b1ffe0e
Aleksandr Trostyanko <ce-aleksandr.trostyanko@beyondnow.com>
2023-08-09 14:49:13 +0200

    initial

--- src/main/java/factory/BrowserFactory.java
+++ src/main/java/factory/BrowserFactory.java
@@ -57,7 +57,7 @@ public class BrowserFactory {
         chromeOptions.addArguments("--silent");
         chromeOptions.addArguments("--start-maximized");
         chromeOptions.addArguments("--incognito");
-        chromeOptions.setHeadless(true);
+        chromeOptions.addArguments("--headless=new");
 
         return chromeOptions;
     }
